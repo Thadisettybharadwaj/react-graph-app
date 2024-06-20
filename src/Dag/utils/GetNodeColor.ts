f41fbdf84f2ffb03d8032bc501aafc61f0cb61b3
Thadisettybharadwaj <thadisettybharadwaj@gmail.com>
2024-06-20 10:56:25 +0530

    giving extra contrast for nodes

--- src/Dag/utils/GetNodeColor.ts
+++ src/Dag/utils/GetNodeColor.ts
@@ -1,3 +1,5 @@
 export const getNodeColor = (n: number) => {
-  return "#" + ((n * 1234567) % Math.pow(2, 24)).toString(16).padStart(6, "0");
+  return (
+    "#" + ((n * 1234567) % Math.pow(n + 3, 24)).toString(16).padStart(6, "0")
+  );
 };
